4b0d7921ee98b2e67536b86f0cedb23a6b53fb44
WillHoll <willhollingsworth15@yahoo.com>
2019-11-19 19:15:14 -0700

    redux framework in place

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+
+    }
+  }
+  
   render() {
     return (
       <div>
@@ -10,4 +18,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(reduxState) {
+  const {username, profile_pic} = reduxState
+  return {
+    username,
+    profile_pic
+  }
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ src/ducks/reducer.js
@@ -0,0 +1,22 @@
+const initialState ={
+  username: '',
+  profile_pic: '',
+  landingAuth: false,
+  newsAuth: false,
+  eventsAuth: false,
+  mediaAuth: false
+};
+
+//ACTION CONSTANTS
+
+
+//ACTION BUILDERS
+
+
+//REDUCER FUNCTION
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+
+    default: return state;
+  };
+};
