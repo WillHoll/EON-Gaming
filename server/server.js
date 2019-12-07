32f303d92459f7833553b4d901b9651cea372bbb
WillHoll <willhollingsworth15@yahoo.com>
2019-12-07 01:44:35 -0700

    fixed a thing

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24
   }
 }));
-
+app.use(express.static(`${__dirname}/../build`));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../build/index.html'));
 // });
